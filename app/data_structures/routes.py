003e06c1c8c90a830c11785d3d2600ea099a301f
Vishnu Raveendran <vishnunilambur@gmail.com>
2020-09-01 23:32:46 +0530

    Updated with new endpoints in routes.py Added and updated html files

--- app/data_structures/routes.py
+++ app/data_structures/routes.py
@@ -12,6 +12,24 @@ def ds_home():
     #     "name": item['display_name']
     # }]
     data_structure = [{
-        "name": "Linked List"
+        "display_name": "Linked List",
+        "name": "linked_list",
+        "gist": "361d250a135c165524d44ccdc7b7e128"
+    }, {
+        "display_name": "Double Linked List",
+        "name": "double_linked_list",
+        "gist": "e9c5e374a9f17d5536196283168d6758"
+    }, {
+        "display_name": "Queue",
+        "name": "queue",
+        "gist": "40ad8fa6bcd9d99348f1d334578307ce"
     }]
     return render_template('ds_main.html', data=data_structure)
+
+
+@ds.route('/datastructures/<string:d_s>')
+def test_route(d_s):
+    gist_link = "https://gist.github.com/vishnurvn/{}.js".format(d_s)
+    return render_template('data_structure_detail.html', link={
+        "link": gist_link
+    })
